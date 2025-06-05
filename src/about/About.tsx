6b65e0c09cb687c7f86f8bfc483d054ec04f4cdf
Damien <d.gervy@free.fr>
2025-06-05 23:02:27 +0200

    added text to the about me section - Not final

--- src/about/About.tsx
+++ src/about/About.tsx
@@ -3,22 +3,22 @@ import { AboutDetail } from './AboutDetail'
 
 export function About() {
     let lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-    let generalDescription = "I am an software engineer graduate looking for a job."
-    let hobbiesDescription = "I like to shower"
-    let skillsDescription = "I am good at coding"
+    let generalDescription = "I am a software engineer with a degree in computer science and I am currently looking for a position as a web developer.\nI strive for quality and am looking to join a team to develop my skills in."
+    let hobbiesDescription = "My interests include games, piano and cat petting.\nI also love exploring new places and learning stuff. But behind the scenes is where I like to look. Finding out how things come to be is what piques my curiosity."
+    let skillsDescription = "Ever since I was a kid I have been fiddling around breaking out things like my favorite websites and games, to try and understand the way they're structured.\nNow I simply code. I am good at coding."
     return (
         <div className='section about' id="about">
             <h1>About me</h1>
             <AboutDetail
-                description={lorem}
+                description={generalDescription}
                 reversed={false}
             />
             <AboutDetail
-                description={lorem}
+                description={hobbiesDescription}
                 reversed={true}
             />
             <AboutDetail
-                description={lorem}
+                description={skillsDescription}
                 reversed={false}
             />
         </div>
