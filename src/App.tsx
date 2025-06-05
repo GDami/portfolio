ebceec45cd4f55a85cc8110c481c1acbd265a348
Damien <d.gervy@free.fr>
2025-06-05 23:01:48 +0200

    App.tsx cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Boiler from './Boiler'
 import { Header } from './header/Header'
 import { Landing } from './landing/Landing'
 import { About } from './about/About'
 
+
+
+
 function App() {
-	const [count, setCount] = useState(0)
 
 	return (
 		<>
