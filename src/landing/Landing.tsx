1828b520df78f0319cada69ea470ffe14d0979e5
Damien <d.gervy@free.fr>
2025-06-06 20:34:45 +0200

    added monkey image

--- src/landing/Landing.tsx
+++ src/landing/Landing.tsx
@@ -1,9 +1,11 @@
 import './Landing.css'
+import homeImg from '/monkey-desktop.png'
 
 export function Landing() {
     return (
         <div className="landing" id="home">
             <h1>Welcome to my website.</h1>
+            <img className="home-img" alt="home-img" src={homeImg}></img>
             <h3>Have fun browsing it !</h3>
         </div>
     )
