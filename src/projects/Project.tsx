a22197d28a626b3b2052849679dc056cd8317f7b
Damien <d.gervy@free.fr>
2025-06-10 11:30:01 +0200

    added projects, added css vars to streamline style across the page

--- src/projects/Project.tsx
+++ src/projects/Project.tsx
@@ -1,7 +1,26 @@
-export function Project() {
+import "./Project.css"
+
+interface ProjectProps {
+    title:string,
+    description:string,
+    url:string,
+    image:string
+}
+
+export function Project(props:ProjectProps) {
+    let imageJsx = (
+        <img className="project-img" src={props.image} alt="project-img"></img>
+    )
+
     return (
-        <>
-            
-        </>
+        <div className="project">
+            <h3 className="project-title">{props.title}</h3>
+            { props.url ? (
+                <a href={props.url} target="_blank">{imageJsx}</a>
+            ) : imageJsx}
+            <span className="project-description">
+                {props.description}
+            </span>
+        </div>
     )
 }
--- src/projects/Projects.tsx
+++ src/projects/Projects.tsx
@@ -1,13 +1,34 @@
 import { Project } from "./Project";
+import suiviImg from "/suivi-preview.png"
+import randomImg from "/random-preview.png"
+import modloaderImg from "/modloader.png"
+import "./Projects.css"
 
 export function Projects() {
     return (
         <div className="section projects" id="projects">
             <h1>Projects</h1>
             <h3></h3>
-            <Project></Project>
-            <Project></Project>
-            <Project></Project>
+            <div className="projects-list">
+                <Project
+                    title="Suivi Candidatures"
+                    description="Suivi Candidatures (French for 'application tracking') is a tool I developed to help me keep track of all the applications I've sent (what a world we live in)."
+                    url="https://github.com/GDami/SuiviCandidatures/"
+                    image={suiviImg}
+                ></Project>
+                <Project
+                    title="Godot Mod Loader"
+                    description="A mod loader for games made in Godot, written in gdscript. This is an open source project I contributed on, and it has been officially added to several games already."
+                    url="https://github.com/GodotModding/godot-mod-loader"
+                    image={modloaderImg}
+                ></Project>
+                <Project
+                    title="random spotify button"
+                    description="A simple app I made to familiarize myself with Angular. This app gives you a random track with the press of a button. It uses the Spotify API."
+                    url="https://github.com/GDami/random-spotify/"
+                    image={randomImg}
+                ></Project>
+            </div>
         </div>
     )
 }
