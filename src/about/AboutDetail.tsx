f89da0020a998532d5ce67aa0d3b477119f6e18b
Damien <d.gervy@free.fr>
2025-06-10 13:26:24 +0200

    new emphasis scheme

--- src/about/AboutDetail.tsx
+++ src/about/AboutDetail.tsx
@@ -6,12 +6,27 @@ interface AboutProps {
     svg:string
 }
 
+function includeEmphasis(text:string) {
+    let textArray = text.split(",,,")
+    return (
+        <>
+            {textArray.map((value, index) => {
+                if (index % 2 === 0) {
+                    return value
+                } else {
+                    return (<em key={"em" + index}>{value}</em>)
+                }
+            })}
+        </>
+    )
+}
+
 export function AboutDetail(props: AboutProps) {
     return (
         <div className={"about-detail " + (props.reversed ? "reversed" : "")}>
             <img className="about-img" src={props.svg} alt="svg img"></img>
             <div className="about-divider" />
-            <span className="about-description">{props.description}</span>
+            <span className="about-description">{includeEmphasis(props.description)}</span>
         </div>
     )
 }
