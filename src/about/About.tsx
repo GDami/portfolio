13f8120bff0fc154567f1db4f1e1543be49e3ef7
Damien <d.gervy@free.fr>
2025-06-17 20:30:50 +0200

    about me text update

--- src/about/About.tsx
+++ src/about/About.tsx
@@ -7,7 +7,8 @@ import hobbiesSvg from "/github.svg"
 export function About() {
     //@ts-expect-error
     let lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-    let generalDescription = "I am a ,,,software engineer,,, with a degree in computer science and I am currently looking for a position as a web developer.\nI strive for quality and am ,,,looking to join a team,,, to share my skills with."
+    let generalDescription = "I am a passionate ,,,software engineer,,, with a degree in computer science and I am currently looking for a position as a web developer.\nI strive for quality and am ,,,looking to join a team,,, to share my skills with."
+    let workDescription = "I am adamant about creating ,,,high-quality, user-focused solutions,,,. Every project is an opportunity to ,,,learn, grow,,,, pour my ,,,dedication,,, and ,,,skills,,, into delivering a ,,,valuable experience,,,."
     let skillsDescription = "I am most proficient using ,,,React,,, and ,,,Java,,,, but I'm also familiar with other tools like ,,,Django, Angular, Nodejs,,, and ,,,SQL,,,.\nMore importantly, I know of ,,,multiple ways,,, to center a div."
     let hobbiesDescription = "My interests include games, piano and ,,,cat petting,,,. I also love exploring new places and learning stuff.\nBut behind the scenes is where I like to look. ,,,Figuring out,,, how things come to be is what piques my curiosity."
     return (
@@ -19,7 +20,7 @@ export function About() {
                 svg={meSvg}
             />
             <AboutDetail
-                description={skillsDescription}
+                description={workDescription}
                 reversed={true}
                 svg={skillsSvg}
             />
