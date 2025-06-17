d9d6a2a472eb45d9f288e63dc384652eeb2cbdbb
Damien <d.gervy@free.fr>
2025-06-17 20:30:07 +0200

    contact section redesign

--- src/contact/Contact.tsx
+++ src/contact/Contact.tsx
@@ -1,20 +1,25 @@
 import "./Contact.css"
+import githubImg from "/github-filled.svg"
+import linkedinImg from "/linkedin.svg"
+import mailImg from "/mail.svg"
 
 export function Contact() {
     return (
         <div className="section contact" id="contact">
             <h1>Contact</h1>
+            <div className="contact-divider"></div>
             <div className="contact-container">
-                <h3>You can find me on</h3>
+                <h3>Feel free to reach out !</h3>
+                <div className="contact-divider"></div>
                 <ul className="contact-list">
                     <li className="contact-item">
-                        <a href="https://github.com/GDami" target="_blank">GitHub</a>
+                        <a href="https://github.com/GDami" target="_blank"><img src={githubImg}></img></a>
                     </li>
                     <li className="contact-item">
-                        <a href="https://www.linkedin.com/in/damien-gervy/" target="_blank">Linkedin</a>
+                        <a href="mailto:gervydamien@gmail.com" target="_blank"><img src={mailImg}></img></a>
                     </li>
                     <li className="contact-item">
-                        <a href="mailto:gervydamien@gmail.com" target="_blank">gervydamien@gmail.com</a>
+                        <a href="https://www.linkedin.com/in/damien-gervy" target="_blank"><img src={linkedinImg}></img></a>
                     </li>
                 </ul>
             </div>
