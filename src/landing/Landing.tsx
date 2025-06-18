9ef03c5efba440ab2cc6dfc4f97f4f7fc143e920
Damien <d.gervy@free.fr>
2025-06-18 09:49:58 +0200

    text and padding change to landing page

--- src/landing/Landing.tsx
+++ src/landing/Landing.tsx
@@ -7,7 +7,7 @@ export function Landing() {
             <h1>Hello, I'm Damien Gervy.</h1>
             <img className="home-img" alt="home-img" src={homeImg}></img>
             <h3>I'm a fullstack web developer based in France.</h3>
-            <h3>Scroll down to know more !</h3>
+            <h3>↓ Scroll down to know more ↓</h3>
         </div>
     )
 }
