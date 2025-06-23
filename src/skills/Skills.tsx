5cd919879b0468ab754a229be3a5d90bc2a6f628
Damien <d.gervy@free.fr>
2025-06-23 18:16:25 +0200

    added skills

--- src/skills/Skills.tsx
+++ src/skills/Skills.tsx
@@ -10,9 +10,15 @@ export function Skills() {
                 <SkillIcon name="CSS3" class="bxl bx-css3" level={2}></SkillIcon>
                 <SkillIcon name="TypeScript" class="bxl bx-typescript" level={3}></SkillIcon>
                 <SkillIcon name="React.js" class="bxl bx-react" level={2}></SkillIcon>
+                <SkillIcon name="Angular" class="bxl bx-angular" level={1}></SkillIcon>
                 <SkillIcon name="Java" class="bxl bx-java" level={2}></SkillIcon>
+                <SkillIcon name="PostgreSQL" class="bxl bx-postgresql" level={2}></SkillIcon>
                 <SkillIcon name="python" class="bxl bx-python" level={1}></SkillIcon>
                 <SkillIcon name="django" class="bxl bx-django" level={1}></SkillIcon>
+                <SkillIcon name="git" class="bxl bx-git" level={2}></SkillIcon>
+                <SkillIcon name="Github" class="bxl bx-github" level={3}></SkillIcon>
+                <SkillIcon name="> bash" class="bxl bx-bash" level={2}></SkillIcon>
+                <SkillIcon name="English" class="bx bx-discussion" level={4}></SkillIcon>
             </div>
         </div>
     )
