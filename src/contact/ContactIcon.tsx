f6fbc316010f7a7922ff1a3e08661b8798cf4eb0
Damien <d.gervy@free.fr>
2025-06-25 11:45:38 +0200

    contacticon can use inline svg

--- src/contact/ContactIcon.tsx
+++ src/contact/ContactIcon.tsx
@@ -1,14 +1,17 @@
+import type { PropsWithChildren } from 'react'
 import './ContactIcon.css'
 
-type ContactIconProps = {
+type ContactIconProps = PropsWithChildren & {
     class: string,
     link: string
 }
 
 export function ContactIcon(props:ContactIconProps) {
+
+    const inside = props.children ? props.children : < i className={props.class}  ></i> 
     return (
         <a className='contact-icon' href={props.link} target="_blank">
-            <span>< i className={props.class}  ></i> </span>
+            <span>{inside}</span>
         </a>
     )
 }
