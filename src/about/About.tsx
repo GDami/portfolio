aa1f9acd318009f877c5cc0d52b74c9bf4155f3f
Damien <d.gervy@free.fr>
2025-06-27 15:19:20 +0200

    changed the about styling

--- src/about/About.tsx
+++ src/about/About.tsx
@@ -7,16 +7,17 @@ export function About() {
     let generalDescription = "I'm a passionate software engineer with a degree in computer science and I am currently looking for a position as a web developer.\nI started out as a script kiddie and have experience in both fullstack web development and game dev. I strive for quality and am always happy to share my skills."
     return (
         <div className='section-container about' id="about">
-            <img className="about-portrait" src={portraitSvg} alt='portrait'></img>
-            <div className='about-details'>
-                <h1 className='main-heading section-title'>About <span>me</span></h1>
-                <br></br>
-                <h2>&gt; whoami<span>_</span></h2>
-                <p>{generalDescription}</p>
-                <br></br>
-                <h2>&gt; cat hobbies.txt<span>_</span></h2>
-                <p>My other interests include <b>piano</b>, <u>biking</u> and <em>cat petting</em>. I love exploring new places, learning stuff and misusing html tags. Generally, behind the scenes is where I like to look. Figuring out how something works is what excites me.</p>
-                
+            <h1 className='main-heading section-title'>About <span>me</span></h1>
+            <div className='about-container'>
+                <img className="about-portrait" src={portraitSvg} alt='portrait'></img>
+                <div className='about-details'>
+                    <h2>&gt; whoami<span>_</span></h2>
+                    <p>{generalDescription}</p>
+                    <br></br>
+                    <h2>&gt; cat hobbies.txt<span>_</span></h2>
+                    <p>My other interests include <b>piano</b>, <u>biking</u> and <em>cat petting</em>. I love exploring new places, learning stuff and misusing html tags. Generally, behind the scenes is where I like to look. Figuring out how something works is what excites me.</p>
+                    
+                </div>
             </div>
         </div>
     )
