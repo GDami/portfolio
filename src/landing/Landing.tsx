cfa6c17d5b237eb2c15b392476e356c0f4a9b468
Damien <d.gervy@free.fr>
2025-06-27 12:02:09 +0200

    replaced the monkey with a sober greeting

--- src/landing/Landing.tsx
+++ src/landing/Landing.tsx
@@ -1,6 +1,5 @@
 import { ContactIcon } from '../contact/ContactIcon'
 import './Landing.css'
-import homeImg from '/monkey-desktop.png'
 
 const codinGameSvg = (<svg clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 2083 2083" xmlns="http://www.w3.org/2000/svg" width="2500" height="2500"><g fill="currentColor"><path d="M0 1636c411.554-97.771 692.897-283.177 724-637 10.714-131.212 67.364-243.777 216-319 207.675-80.617 305.728-52.164 390-10 92.668 81.722 119.468 199.94 50 371.5-31.914 100.266-291.001 223.867-410 258.5-392.539 175.893-595.504 430.282-561 783H0z"/><ellipse cx="1666.5" cy="408" rx="196.5" ry="192"/></g></svg>)
 
@@ -26,7 +25,7 @@ export function Landing() {
                     <ContactIcon link='https://leetcode.com/u/DamienDamien/' class='bxl  bx-leetcode'></ContactIcon>
                 </div>
             </div>
-            <img className="home-img" alt="home-img" src={homeImg}></img>
+            <span className='home-img'>&gt;hi!<span>_</span></span>
         </div>
     )
 }
