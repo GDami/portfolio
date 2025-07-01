2868feb8d10af7bb672eed1bbec721c9d05d7158
Damien <d.gervy@free.fr>
2025-07-02 01:54:30 +0200

    responsive hero header

--- src/landing/Hero.tsx
+++ src/landing/Hero.tsx
@@ -13,7 +13,7 @@ export function Hero() {
             <div className='hero-heading'>
                 <h1 className='hero-title'>Hey, I'm Damien <span className='wave-hand'>👋</span></h1>
                 <h3 className='hero-info'><i className='bxr  bx-location'  ></i> Paris, France</h3>
-                <h3 className='hero-subtext'>I'm a <span>junior frontend developer</span> who creates user-focused experiences, giving special attention to detail.</h3>
+                <h3 className='hero-subtext'>I'm a <span>junior frontend developer</span> who creates user-focused experiences, and gives special attention to detail.</h3>
                 <div className='hero-ctas'>
                     <a className='hero-cta primary animated-underline' href='#projects'>Check my projects</a>
                     <a className='hero-cta secondary animated-underline' href='#about'>More about me</a>
