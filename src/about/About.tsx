461893e093f0a2057b59ed3d6640e167116ca004
GDami <41579255+GDami@users.noreply.github.com>
2025-07-03 02:21:34 +0200

    larger cta, changed the detail interaction

--- src/about/About.tsx
+++ src/about/About.tsx
@@ -12,7 +12,7 @@ export function About() {
                     <p>I'm a <em>software engineer</em> with a degree in computer science and I am currently looking for a position as a <em>frontend or fullstack developer</em>. I started out as a script kiddie and have experience in both fullstack web development and game dev. I strive for <em>quality</em> and am always happy to <em>share my skills</em>.</p>
                     <br></br>
                     <h2>&gt; education<span>_</span></h2>
-                    <p>I attended and graduated from the <span><a href='https://www.utt.fr/formations/diplome-d-ingenieur/reseaux-et-telecommunications/'>University of Technology of Troyes</a></span> in 2021. This is where I took on my first large <em>coding projects</em>, and learned useful tools like <em>algorithms</em>, <em>statistics</em> or <em>geometry</em>.</p>
+                    <p>I attended and graduated from the <span><a href='https://www.utt.fr/formations/diplome-d-ingenieur/reseaux-et-telecommunications/' target="_blank">University of Technology of Troyes</a></span> in 2021. This is where I took on my first large <em>coding projects</em>, and learned useful tools like <em>algorithms</em>, <em>statistics</em> or <em>geometry</em>.</p>
                     <br></br>
                     <h2>&gt; hobbies<span>_</span></h2>
                     <p>My other interests include <em>piano</em>, <em>biking</em>, <em>cooking</em>, <em>games</em> and <em>cat petting</em>. I love <em>exploring</em> and <em>learning</em>. I also find contentment in figuring out how things work, be it through <em>research</em> or <em>experimenting</em>.</p>
