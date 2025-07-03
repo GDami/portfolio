4171eb1f855032718cb8dbdbfd515f70fb074cc0
Damien <d.gervy@free.fr>
2025-07-03 13:05:48 +0200

    reverted to the 800px width, changed the header hover style and readded the .scrolled behavior

--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -5,10 +5,12 @@ import resumePDF from '/Resume Damien Gervy Front End.pdf'
 
 
 let html:HTMLElement
+let header:HTMLElement
 let darkToggle:HTMLElement
 
 function initHtmlElements() {
     html = document.documentElement
+    header = document.getElementById("header")!
     darkToggle = document.getElementById("dark-toggle")!
 }
 
@@ -25,10 +27,22 @@ function toggleDarkMode() {
     }
 }
 
+function handleScroll(this: Window, _ev: Event) : any {
+    if (this.scrollY > 1) {
+        header.classList.add("scrolled")
+    } else {
+        header.classList.remove("scrolled")
+    }
+}
+
 export function Header() {
     useEffect(() => {
         initHtmlElements()
+        window.addEventListener('scroll', handleScroll, {passive: true})
+
+        return () => window.removeEventListener('scroll', handleScroll)
     }, [])
+
 
     return (
         <header id="header">
@@ -38,26 +52,28 @@ export function Header() {
                     < i className='bx  bx-home-alt'  ></i> 
                 </a>
                 <div className='header-filler'></div>
-                <a href='#about'>
-                    <span>About</span>
-                    < i className='bx  bx-info-square'  ></i> 
-                </a>
-                <a href='#projects'>
-                    <span>Projects</span>
-                    < i className='bx  bx-chart-gantt'  ></i> 
-                </a>
-                <a href='#skills'>
-                    <span>Skills</span>
-                    < i className='bx  bx-brush'  ></i> 
-                </a>
-                <a href='#experiences'>
-                    <span>Experience</span>
-                    < i className='bx  bx-briefcase-alt-2'  ></i> 
-                </a>
-                <a href='#contact'>
-                    <span>Contact</span>
-                    < i className='bx  bx-contact-book'  ></i> 
-                </a>
+                <div className='nav-center'>
+                    <a href='#about'>
+                        <span>About</span>
+                        < i className='bx  bx-info-square'  ></i> 
+                    </a>
+                    <a href='#projects'>
+                        <span>Projects</span>
+                        < i className='bx  bx-chart-gantt'  ></i> 
+                    </a>
+                    <a href='#skills'>
+                        <span>Skills</span>
+                        < i className='bx  bx-brush'  ></i> 
+                    </a>
+                    <a href='#experiences'>
+                        <span>Experience</span>
+                        < i className='bx  bx-briefcase-alt-2'  ></i> 
+                    </a>
+                    <a href='#contact'>
+                        <span>Contact</span>
+                        < i className='bx  bx-contact-book'  ></i> 
+                    </a>
+                </div>
                 <div className='header-filler'></div>
                 <a className="bx-sun" id='dark-toggle' onClick={toggleDarkMode}></a>
                 <a className='resume-link' href={resumePDF} target='_blank'>
