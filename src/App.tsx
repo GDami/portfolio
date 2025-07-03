a084df127b4e72fef3fe3878135335c66b6848a7
Damien <d.gervy@free.fr>
2025-07-03 17:30:12 +0200

    fade in for every section

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,28 @@ import { Projects } from './projects/Projects'
 import { Contact } from './contact/Contact'
 import { Skills } from './skills/Skills'
 import { Experiences } from './experiences/Experiences'
+import { useEffect } from 'react'
 
 
 
 
 function App() {
+
+	useEffect(() => {
+		const sections = document.querySelectorAll("section")
+
+		const observer = new IntersectionObserver(entries => {
+			entries.forEach(entry => {
+				if (entry.isIntersecting) {
+					entry.target.classList.add("shown")
+				}
+			})
+		}, { rootMargin: "-100px"})
+		
+		sections.forEach(section => observer.observe(section))
+
+		return () => observer.disconnect()
+	})
 
 	return (
 		<>
