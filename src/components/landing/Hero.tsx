91d01e5cd50cd52f75b4ca46609cc4b537e31258
Damien <d.gervy@free.fr>
2025-07-08 15:38:28 +0200

    put the hero img between the title and subtext in mobile mode

--- src/components/landing/Hero.tsx
+++ src/components/landing/Hero.tsx
@@ -14,15 +14,15 @@ export function Hero() {
                     <img className='hero-img' src={heroImg} alt='hero-img'></img>
                     <img className='villain-img' src={versoImg} alt='noisette-img'></img>
                 </div>
-                <div className='hero-heading'>
-                    <h1 className='hero-title'>{t("hero.title.1")}<span className='no-linebreak'>{t("hero.title.2")}<span className='wave-hand'>👋</span></span></h1>
-                    <h3 className='hero-info'><i className='bxr  bx-location'  ></i> Paris, France</h3>
-                    <h3 className='hero-subtext'>{t("hero.subtext.1")}<strong>{t("hero.subtext.2")}</strong>{t("hero.subtext.3")}<span className='hero-detail animated-underline'>{t("hero.subtext.4")}</span>.</h3>
-                    <div className='hero-ctas'>
-                        <a className='hero-cta secondary ' href='#about'><span className='animated-underline'>{t("hero.cta.1")}</span></a>
-                        <a className='hero-cta primary ' href='#projects'><span className='animated-underline'>{t("hero.cta.2")}</span></a>
-                    </div>
+                <h1 className='hero-title'>{t("hero.title.1")}<span className='no-linebreak'>{t("hero.title.2")}<span className='wave-hand'>👋</span></span></h1>
+                <h3 className='hero-info'><i className='bxr  bx-location'  ></i> Paris, France</h3>
+                <h3 className='hero-subtext'>{t("hero.subtext.1")}<strong>{t("hero.subtext.2")}</strong>{t("hero.subtext.3")}<span className='hero-detail animated-underline'>{t("hero.subtext.4")}</span>.</h3>
+                <div className='hero-ctas'>
+                    <a className='hero-cta secondary ' href='#about'><span className='animated-underline'>{t("hero.cta.1")}</span></a>
+                    <a className='hero-cta primary ' href='#projects'><span className='animated-underline'>{t("hero.cta.2")}</span></a>
                 </div>
+                {/* <div className='hero-heading'>
+                </div> */}
             </div>
         </div>
     )
