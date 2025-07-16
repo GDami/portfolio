a860b4d1ab6fabe0bc0dc456139652732927a933
Damien <d.gervy@free.fr>
2025-07-16 23:53:46 +0200

    more emphasises on the hero subtext

--- src/components/landing/Hero.tsx
+++ src/components/landing/Hero.tsx
@@ -2,7 +2,7 @@ import heroImg from '/hero.jpg'
 import versoImg from '/noisette.jpg'
 
 import './Hero.css'
-import { useTranslation } from 'react-i18next'
+import { Trans, useTranslation } from 'react-i18next'
 
 export function Hero() {
     const { t } = useTranslation()
@@ -16,7 +16,11 @@ export function Hero() {
                 </div>
                 <h1 className='hero-title'>{t("hero.title.1")}<span className='no-linebreak'>{t("hero.title.2")}<span className='wave-hand'>👋</span></span></h1>
                 <h3 className='hero-info'><i className='bxr  bx-location'  ></i> Paris, France</h3>
-                <h3 className='hero-subtext'>{t("hero.subtext.1")}<strong>{t("hero.subtext.2")}</strong>{t("hero.subtext.3")}<span className='hero-detail animated-underline'>{t("hero.subtext.4")}</span>.</h3>
+                <h3 className='hero-subtext'>
+                    <Trans i18nKey="hero.subtext">
+                        I'm a junior <strong>frontend developer</strong> who cares about <strong>user experience</strong> and <strong className='hero-detail animated-underline'>detail</strong>.
+                    </Trans>
+                </h3>
                 <div className='hero-ctas'>
                     <a className='hero-cta secondary ' href='#about'><span className='animated-underline'>{t("hero.cta.1")}</span></a>
                     <a className='hero-cta primary ' href='#projects'><span className='animated-underline'>{t("hero.cta.2")}</span></a>
