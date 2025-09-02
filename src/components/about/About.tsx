7326278f5325be8ec73f4b2f77ee7ee339a3a213
Damien <d.gervy@free.fr>
2025-09-02 17:02:44 +0200

    changement du texte pour le 3e paragraphe de about

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -10,7 +10,7 @@ export function About() {
             <div className='about-container'>
                 <div className='about-details'>
                     <div className='about-detail'>
-                        <h2>&gt; whoami<span>_</span></h2>
+                        <h2>&gt; {t("whoami.title")}<span>_</span></h2>
                         <p>
                             <Trans i18nKey="whoami.description">
                                 I'm a <em>software engineer</em> with a degree in <em>computer science</em> and I am currently looking for opportunities to work as a <em>frontend or fullstack developer</em>. I started out as a script kiddie and have experience in both fullstack web development and game dev. I strive for <em>quality</em> and am always happy to <em>share my skills</em>.
